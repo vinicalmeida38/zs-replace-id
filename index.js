1c10be0589f4e3abdea900c5892d1fb677c2e067
Vinicius Almeida <vinicius.almeida@ab-inbev.com>
2023-02-16 10:11:41 -0300

    Updating zephyr scale prefix

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
-const json = require("./testCasesKey.json");
-const config = require("./config.json");
-const fs = require("fs");
-const readline = require("readline");
-const glob = require("glob");
+const json = require('./testCasesKey.json');
+const config = require('./config.json');
+const fs = require('fs');
+const readline = require('readline');
+const glob = require('glob');
 
 const replaceId = () => {
   glob(config.testCasesFolder, (err, files) => {
-      files.map((file) => {
-        const ws = fs.createWriteStream(file, {
-            flags: "r+",
-            defaultEncoding: "utf8",
-          });
-      
-          const rl = readline.createInterface({
-            input: fs.createReadStream(file),
-          });
-      
-          rl.on("line", (line) => {
-            json.testCasesKey.map((testCase) => {
-              let regex = new RegExp("\\" + "b" + testCase.BEESSI + "\\" +"b")
-              if (line.search(regex) >= 0) {
-                console.log(`Replacing ${testCase.BEESSI}`);
-                line = line.replace(testCase.BEESSI, testCase.BEESPAE);
-              }
-            });
-            ws.write(line + '\n');
-          });
-      
-          rl.on("close", function () {
-            ws.close();
-          });
-      })
-  
+    files.map((file) => {
+      const ws = fs.createWriteStream(file, {
+        flags: 'r+',
+        defaultEncoding: 'utf8',
+      });
+
+      const rl = readline.createInterface({
+        input: fs.createReadStream(file),
+      });
+
+      rl.on('line', (line) => {
+        json.testCasesKey.map((testCase) => {
+          let regex = new RegExp('\\' + 'b' + testCase.BEESPAE + '\\' + 'b');
+          if (line.search(regex) >= 0) {
+            console.log(`Replacing ${testCase.BEESPAE}`);
+            line = line.replace(testCase.BEESPAE, testCase.BEESQM);
+          }
+        });
+        ws.write(line + '\n');
+      });
+
+      rl.on('close', function () {
+        ws.close();
+      });
+    });
   });
 };
 
