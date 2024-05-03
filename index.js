103b66ec2746af896f84c95a300c5b53ecb6a4e0
Breno Cruz <breno.cruz@ab-inbev.com>
2024-05-03 12:17:47 -0300

    Partial EOL implementation in writeStream

--- index.js
+++ index.js
@@ -1,47 +1,50 @@
 const config = require("./config.json");
 const fs = require("fs");
 const readline = require("readline");
-const glob = require("glob");
+const os = require("os");
 const { scanProjectFiles } = require("./ getProjectFilesAndReplaceIds");
-const { getKeyToKeyComparison, getOriginToTargetKeyComparison } = require("./zephyrCsvUtils");
+const {
+  getKeyToKeyComparison,
+  getOriginToTargetKeyComparison,
+} = require("./zephyrCsvUtils");
 
-const ORIGIN_TO_TARGET_KEY_COMPARISON = getOriginToTargetKeyComparison()
 
-const processLineFunction = (writeStream,line) => {
+const ORIGIN_TO_TARGET_KEY_COMPARISON = getOriginToTargetKeyComparison();
+
+const processLineFunction = (writeStream, line) => {
   ORIGIN_TO_TARGET_KEY_COMPARISON.map((keys) => {
-  const {from: fromId,to:toId} = keys
-  console.log(keys)
-  const FROM_ID_CHECK_REGEX = new RegExp(`\\b${fromId}\\b`);
+    const { from, to } = keys;
+    const [fromProjectId, fromId] = from.split("-");
+    const [toProjectId, toId] = to.split("-");
+    const FROM_ID_CHECK_REGEX = new RegExp(`${fromId}`);
 
-  const fromIdExists = line.search(FROM_ID_CHECK_REGEX) >= 0;
-  if (fromIdExists) {
-    console.log(`Replacing ${fromId} with ${toId}`);
-    line = line.replace(fromId, toId);
-  }
-});
-writeStream.write(`${line}\n`);
+    const fromIdExists = line.search(FROM_ID_CHECK_REGEX) >= 0;
+    if (fromIdExists) {
+      console.log(`Replacing ${fromId} with ${toId}`);
+      line = line.replace(fromId, toId);
+      line = line.replace(fromProjectId, toProjectId);
+    }
+  });
+    writeStream.write(`${line}${os.EOL}`);
+
 };
 
 const processFile = (file) => {
-
   const writeStream = fs.createWriteStream(file, {
     flags: "r+",
     defaultEncoding: "utf8",
   });
 
   const readLine = readline.createInterface({
-    input: fs.createReadStream(file),
+    input: fs.createReadStream(file), 
   });
 
-  readLine.on("line", (line) => processLineFunction(writeStream,line));
+  readLine.on("line", (line) => processLineFunction(writeStream, line));
   readLine.on("close", () => writeStream.close());
 };
 
-
-
 const execute = () => {
-  scanProjectFiles(processFile)
+  scanProjectFiles(processFile);
 };
 
-
-execute()
+execute();
